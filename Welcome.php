07e1fbc5b0c08c66e0f1c134c8f4e2f4f25209c9
StijnCaius <scvanveen@hotmail.com>
2016-12-12 12:09:47 +0100

    welcome script php

---
+++ Welcome.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Welkom op onze site, <?php echo $_POST["name"]! ;?> <br>
+We nemen zo snel mogelijk contact met je op onder het volgende email adres: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
